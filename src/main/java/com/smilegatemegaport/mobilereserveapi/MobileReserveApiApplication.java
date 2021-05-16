8caa50b3de5d0f7f500bf6e6bf4569c4a4e111bc
minhoe <minhoe1122@gmail.com>
2021-05-17 02:48:38 +0900

    Spring 프로젝트 초기화

---
+++ src/main/java/com/smilegatemegaport/mobilereserveapi/MobileReserveApiApplication.java
@@ -0,0 +1,13 @@
+package com.smilegatemegaport.mobilereserveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileReserveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileReserveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smilegatemegaport/mobilereserveapi/MobileReserveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smilegatemegaport.mobilereserveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileReserveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
